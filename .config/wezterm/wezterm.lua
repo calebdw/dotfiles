0ba72f2f333b29a2c193d483af1faedffeea587f
Caleb White <cdwhite3@pm.me>
2024-02-20 00:28:46 -0600

    feat: install wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require('wezterm');
+local config = wezterm.config_builder()
+
+-- Fonts --
+config.font = wezterm.font_with_fallback({
+  'FiraCode Nerd Font',
+  -- 'JetBrainsMono Nerd Font',
+  'Nonicons',
+})
+
+-- Colors --
+config.color_scheme = 'tokyonight_night'
+
+-- Window --
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "NONE"
+config.enable_scroll_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+return config
