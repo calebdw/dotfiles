11ac1c58272cf2bd301af9f2ffb1231b539d6373
Caleb White <cdwhite3@pm.me>
2024-03-06 09:14:21 -0600

    chore(nvim): properly handle null value in env plugin

--- .config/nvim/lua/user/plugins/envs.lua
+++ .config/nvim/lua/user/plugins/envs.lua
@@ -1,15 +1,17 @@
 return {
   {
     'ellisonleao/dotenv.nvim',
-    opts = {
-      enable_on_load = true,
-    },
+    opts = { enable_on_load = true },
     config = function(_, opts)
       require('dotenv').setup(opts)
 
-      require('dotenv').command({fargs = {
-        vim.fs.dirname(vim.uv.fs_realpath(vim.fn.expand('~/.config/nvim/init.lua'))) .. '/../.env',
-      }})
+      local dotfiles = vim.uv.fs_realpath(vim.fn.expand('~/.config/nvim/init.lua'))
+
+      assert(dotfiles, 'Could not find dotfiles')
+
+      require('dotenv').command({
+        fargs = { vim.fs.dirname(dotfiles) .. '/../.env' }
+      })
     end,
   },
 }
