c13fc900a73e1913fd284aa5ef47a0840d6d3298
Caleb White <cdwhite3@pm.me>
2024-03-06 09:25:52 -0600

    chore(nvim): remove dep ts playground

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -5,19 +5,11 @@ return {
   end,
   opts = {
     ensure_installed = 'all',
-    highlight = {
-      enable = true,
-      -- disable = {"javascript", "typescript"},
-      -- additional_vim_regex_highlighting = true,
-    },
-    -- Needed because treesitter highlight turns off autoindent for php files
-    indent = {
-      enable = true,
-    },
+    highlight = { enable = true, },
+    indent = { enable = true, },
     incremental_selection = {
       enable = true,
-      keymaps = {
-        -- set to `false` to disable one of the mappings
+      keymaps = { -- set to `false` to disable one of the mappings
         init_selection = "gnn",
         node_incremental = "grn",
         scope_incremental = "grc",
@@ -29,15 +21,6 @@ return {
     },
     endwise = { -- 'RRethy/nvim-treesitter-endwise',
       enable = true,
-    },
-    -- 'nvim-treesitter/playground',
-    playground = {
-      enable = true,
-    },
-    query_linter = {
-      enable = true,
-      use_virtual_text = true,
-      lint_events = { 'BufWrite', 'CursorHold' },
     },
     textobjects = { -- 'nvim-treesitter/nvim-treesitter-textobjects',
       select = {
@@ -121,9 +104,8 @@ return {
 
     require('nvim-treesitter.configs').setup(opts)
 
-    map('n', '<leader>pg', '<cmd>TSPlayground<cr>')
-    map('n', '<leader>ph', '<cmd>TSHighlightCapturesUnderCursor<cr>')
-    map('n', '<leader>pn', '<cmd>TSNodeUnderCursor<cr>')
+    map('n', '<leader>it', vim.treesitter.inspect_tree)
+    map('n', '<leader>i', vim.show_pos)
   end,
   dependencies = {
     'JoosepAlviste/nvim-ts-context-commentstring',
@@ -132,6 +114,5 @@ return {
     -- @todo: configure plugin
     'nvim-treesitter/nvim-treesitter-textobjects',
     'nvim-treesitter/nvim-treesitter-context',
-    'nvim-treesitter/playground',
   },
 }
