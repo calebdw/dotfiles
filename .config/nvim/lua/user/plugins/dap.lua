683e74158c09a1c66578e5ccb58f92df20c61e9c
Caleb White <cdwhite3@pm.me>
2024-04-26 11:48:14 -0500

    feat: close dapui on disconnect event

--- .config/nvim/lua/user/plugins/dap.lua
+++ .config/nvim/lua/user/plugins/dap.lua
@@ -65,6 +65,7 @@ return {
 
       dap.listeners.before.attach.dapui_config = function() dapui.open() end
       dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.disconnect.dapui_config = function() dapui.close() end
       dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
       dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
     end,
