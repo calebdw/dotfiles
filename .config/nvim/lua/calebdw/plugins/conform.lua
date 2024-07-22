e5bd45e11808de640c888ec8eb3aa8f33fd024f7
Caleb White <cdwhite3@pm.me>
2024-07-22 12:58:12 -0500

    chore(nvim): update conform plugin

--- .config/nvim/lua/calebdw/plugins/conform.lua
+++ .config/nvim/lua/calebdw/plugins/conform.lua
@@ -10,9 +10,7 @@ return {
     keys = {
       {
         '<leader>f',
-        function()
-          require('conform').format({ async = true, lsp_fallback = true, timeout_ms = 5000 })
-        end,
+        function() require('conform').format({ async = true, lsp_fallback = true, timeout_ms = 5000 }) end,
         mode = '',
         desc = 'Format buffer',
       },
@@ -25,35 +23,27 @@ return {
         },
       },
       formatters_by_ft = {
-        blade = { { 'prettier', 'blade-formatter' } },
-        json = { { 'prettier', 'jq' } },
-        javascript = { { 'prettierd', 'prettier' } },
+        blade = { 'prettier', 'blade-formatter', stop_after_first = true },
+        json = { 'prettier', 'jq', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
         lua = { 'stylua' },
-        php = { { 'pint', 'phpcbf', 'php_cs_fixer' } },
-        sql = { { 'pg_format', 'sqlfmt' } },
+        php = { 'pint', 'phpcbf', 'php_cs_fixer', stop_after_first = true },
+        sql = { 'pg_format', 'sqlfmt', stop_after_first = true },
         ['*'] = { 'injected' },
       },
       format_on_save = function(bufnr)
-        if slow_format_filetypes[vim.bo[bufnr].filetype] then
-          return
-        end
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then return end
         local function on_format(err)
-          if err and err:match('timeout$') then
-            slow_format_filetypes[vim.bo[bufnr].filetype] = true
-          end
+          if err and err:match('timeout$') then slow_format_filetypes[vim.bo[bufnr].filetype] = true end
         end
 
         return { timeout_ms = 500, lsp_fallback = true }, on_format
       end,
       format_after_save = function(bufnr)
-        if slow_format_filetypes[vim.bo[bufnr].filetype] then
-          return
-        end
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then return end
         return { timeout_ms = 5000, lsp_fallback = true }
       end,
     },
-    init = function()
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
+    init = function() vim.o.formatexpr = "v:lua.require'conform'.formatexpr()" end,
   },
 }
