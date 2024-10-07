f5774faf734343f6b3433f9fe74abfbd028a4200
Caleb White <cdwhite3@pm.me>
2024-10-07 12:57:38 -0500

    feat(git): add autocommand to shorten colorcolumn in commit messages

--- .config/nvim/lua/calebdw/autocommands.lua
+++ .config/nvim/lua/calebdw/autocommands.lua
@@ -6,9 +6,7 @@ local file_formats = vim.api.nvim_create_augroup('user_file_formats', { clear = 
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   group = file_formats,
   pattern = '*.qf',
-  callback = function()
-    vim.bo.filetype = 'sh'
-  end,
+  callback = function() vim.bo.filetype = 'sh' end,
 })
 
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
@@ -24,17 +22,13 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
   group = default,
   pattern = '*',
-  callback = function()
-    vim.wo.spell = false
-  end,
+  callback = function() vim.wo.spell = false end,
 })
 
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
   desc = 'Highlight when yanking text',
   group = default,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
 })
 
 vim.api.nvim_create_autocmd({ 'FileType' }, {
@@ -46,6 +40,13 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  desc = 'Git commit message settings',
+  group = default,
+  pattern = { 'gitcommit', 'gitsendemail', 'NeogitCommitMessage' },
+  callback = function() vim.opt_local.colorcolumn = '72' end,
+})
+
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   desc = 'Clean up old files in the state directory.',
   group = default,
@@ -54,14 +55,10 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
     local days = 30
     local cwd = vim.fn.stdpath('state')
 
-    if type(cwd) == 'table' then
-      cwd = cwd[1]
-    end
+    if type(cwd) == 'table' then cwd = cwd[1] end
 
     local function notify_error(msg)
-      vim.schedule(function()
-        vim.notify(msg, vim.log.levels.ERROR)
-      end)
+      vim.schedule(function() vim.notify(msg, vim.log.levels.ERROR) end)
     end
 
     Job:new({
