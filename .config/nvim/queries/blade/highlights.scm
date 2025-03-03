d99d1d373a6804624fea9b3eb3e04a6279fe8f20
Caleb White <cdwhite3@pm.me>
2025-03-03 08:31:32 -0600

    feat(nvim): update blade queries

--- .config/nvim/queries/blade/highlights.scm
+++ .config/nvim/queries/blade/highlights.scm
@@ -1,4 +1,14 @@
-(directive) @tag
-(directive_start) @tag
-(directive_end) @tag
+([
+  (directive)
+  (directive_start)
+  (directive_end)
+] @tag
+  (#set! priority 101))
+
+([
+  (bracket_start)
+  (bracket_end)
+] @tag.delimiter
+  (#set! priority 101))
+
 ((comment) @comment @spell (#set! priority 101))
