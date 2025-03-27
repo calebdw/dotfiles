a5b2ac1c9eced4b667534448fefa4a6316fd4745
Caleb White <cdwhite3@pm.me>
2025-03-27 13:07:42 -0500

    chore(nvim): update dotenv plugin

--- .config/nvim/lua/calebdw/plugins/envs.lua
+++ .config/nvim/lua/calebdw/plugins/envs.lua
@@ -1,17 +1,9 @@
 return {
   {
     'ellisonleao/dotenv.nvim',
-    opts = { enable_on_load = true },
-    config = function(_, opts)
-      require('dotenv').setup(opts)
-
-      local dotfiles = vim.uv.fs_realpath(vim.fn.expand('~/.config/nvim/init.lua'))
-
-      assert(dotfiles, 'Could not find dotfiles')
-
-      require('dotenv').command({
-        fargs = { vim.fs.dirname(dotfiles) .. '/../.env' }
-      })
-    end,
+    opts = {
+      enable_on_load = true,
+      file_name = '~/.config/nvim/.env',
+    },
   },
 }
