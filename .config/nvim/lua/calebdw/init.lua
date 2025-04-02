a39ce8db3e2b2318a58907090392f60d93c50f5e
Caleb White <cdwhite3@pm.me>
2025-04-02 08:45:28 -0500

    chore(nvim): reorganize lua files

--- .config/nvim/lua/calebdw/init.lua
+++ .config/nvim/lua/calebdw/init.lua
@@ -1,27 +1,4 @@
-require('calebdw.autocommands')
+require('calebdw.autocmds')
 require('calebdw.options')
 require('calebdw.keymaps')
-
--- Bootstrap Lazy
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  local repo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', repo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup('calebdw.plugins', {
-  diff = {
-    cmd = 'terminal_git',
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        'netrwPlugin',
-      },
-    },
-  },
-  ui = {
-    border = 'rounded',
-  },
-})
+require('calebdw.lazy')
---
+++ .config/nvim/lua/calebdw/lazy.lua
@@ -0,0 +1,22 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local repo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', repo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+---@type LazyConfig
+local opts = {
+  checker = { enabled = true, },
+  dev = { path = '~/sources/neovim' },
+  diff = {
+    cmd = 'terminal_git',
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  performance = {
+    rtp = { disabled_plugins = { 'netrwPlugin', }, },
+  },
+  ui = { border = 'rounded', },
+}
+
+require('lazy').setup('calebdw.plugins', opts)
