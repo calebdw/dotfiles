bf52fb658b3d9a1a62b37a2cac5107f50144ce2a
Caleb White <cdwhite3@pm.me>
2025-06-16 16:50:28 -0500

    feat(nvim): add actually-doom.nvim plugin

--- .config/nvim/lua/calebdw/plugins/games.lua
+++ .config/nvim/lua/calebdw/plugins/games.lua
@@ -2,4 +2,5 @@ return {
   { 'alec-gibson/nvim-tetris', cmd = 'Tetris' },
   { 'seandewar/nvimesweeper', cmd = 'Nvimesweeper' },
   { 'seandewar/killersheep.nvim', cmd = 'KillKillKill' },
+  { 'seandewar/actually-doom.nvim', cmd = 'Doom' },
 }
