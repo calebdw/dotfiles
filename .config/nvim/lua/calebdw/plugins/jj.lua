38c959fcfdf8b30ed890f9d0d691c69623c51b0e
Caleb White <cdwhite3@pm.me>
2025-06-23 16:46:46 -0500

    chore(nvim): don't show delete count in vcsigns.nvim

--- .config/nvim/lua/calebdw/plugins/jj.lua
+++ .config/nvim/lua/calebdw/plugins/jj.lua
@@ -12,6 +12,7 @@ return {
     'algmyr/vcsigns.nvim',
     opts = {
       target_commit = 1,  -- Nice default for jj with new+squash flow.
+      show_delete_count = false,
       signs = {
         change_delete = { text = '~' },
       },
