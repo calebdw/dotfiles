70ae1b9ecc8f0b2bfc2a62a57af7964691e02501
Caleb White <cdwhite3@pm.me>
2025-07-04 23:13:51 -0500

    feat(opencode): add opencode bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ set -x EDITOR nvim
 fish_add_path --path ./vendor/bin
 fish_add_path --path ./node_modules/bin
 fish_add_path ~/go/bin
+fish_add_path ~/.opencode/bin
 fish_add_path -a ~/.config/composer/vendor/bin
 
 # Aliases
