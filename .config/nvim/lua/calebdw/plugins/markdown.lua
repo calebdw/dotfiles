4ecc692d7c9d4b0a1cca1e50f7fee17578854328
Caleb White <cdwhite3@pm.me>
2026-01-30 15:21:10 -0600

    chore(nvim): removed deprecated glow.nvim

--- .config/nvim/lua/calebdw/plugins/markdown.lua
+++ .config/nvim/lua/calebdw/plugins/markdown.lua
@@ -1,21 +1,6 @@
 ---@type LazySpec
 return {
   {
-    'ellisonleao/glow.nvim',
-    keys = {
-      {
-        '<leader>md',
-        ':w<cr>:Glow<cr>',
-        desc = 'Render markdown',
-      },
-    },
-    opts = {
-      border = 'rounded',
-      pager = false,
-      width = 120,
-    },
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
     dependencies = {
